76a144999612cc8c5a71d17df0dce173d378fdff
Daniel Merritt <daniel.mafou@gmail.com>
2023-09-12 11:13:55 -0400

    wireframe of weather app

--- App.js
+++ App.js
@@ -4,14 +4,21 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 
 export default function App() {
-  console.log("Hello World")
-  const message = 'hello world'
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={ styles.container }>
-        <Text>{message}</Text>
-        <StatusBar style="auto" />
+        <Text style={ styles.temp }>6</Text>
+        <Text style= {styles.feels}>Feels Like 5</Text>
+        <View style= {styles.highLowWrapper}>
+          <Text style= {styles.highLow}>High: 8</Text>
+          <Text style= {styles.highLow}>Low:6</Text>
+        </View>
       </View>
+      <View style= {styles.bodyWrapper}>
+        <Text style={styles.description}>It's Sunny</Text>
+        <Text style= {styles.message}>It's Perfect T-shirt weather</Text>
+      </View>
+        <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -19,11 +26,36 @@ export default function App() {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: 'pink',
   },
   container: {
-    backgroundColor: 'pink',
     flex: 1,
     alignItems: 'center',
-    
+
   },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: '20'
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
 });
